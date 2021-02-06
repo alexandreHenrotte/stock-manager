0790e5060e140ae860d46bdee509d64434ce2fb7
Alexandre Henrotte <alexandre.henrotte1@gmail.com>
2021-02-06 23:20:10 +0100

    Login verification on backend

--- api/routes/login.js
+++ api/routes/login.js
@@ -1,9 +1,37 @@
 var express = require('express');
 const User = require('./../models/User')
+const bcrypt = require('bcrypt');
 var router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send({token : '12345'});
+router.post('/', async (req, res) => {
+    const user = await User.findOne({email:req.body.email});
+    console.log(req.body);
+    if (user === null) {
+        res.status(400).json({
+            success: false,
+            errorMessage: "User not found"
+        })
+    }
+
+    try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            res.status(200).json({
+                success: true,
+                redirectUrl: "/products"
+            });
+        }
+        else {
+            res.status(400).json({
+                sucess: false,
+                errorMessage: "Wrong password"
+            })
+        }
+    } catch {
+        return res.status(500).json({
+            success: false,
+            errorMessage: "Authentication Error"
+        })
+    }
 })
 
 module.exports = router;
