9d1ccb154dedb3ff588f8c0daffec1982782c10c
Alexandre Henrotte <alexandre.henrotte1@gmail.com>
2021-02-13 21:36:47 +0100

    Change local IP to localhost and prettier code

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,75 +1,86 @@
-import React from 'react';
-import styles from './Login.module.css';
+import React from "react";
+import styles from "./Login.module.css";
 
 class Login extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            email : "",
-            password : ""
-        }
-    }
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
 
-    onLoginSuccess = () => {
-        console.log("Succeed !")
-    }
-    onLoginFailure = (errorMessage) => {
-        console.log(errorMessage);
-    }
+  onLoginSuccess = () => {
+    console.log("Succeed !");
+  };
+  onLoginFailure = (errorMessage) => {
+    console.log(errorMessage);
+  };
 
-    setEmail= (event) => {
-        this.setState({email : event.target.value});
-    }
-    setPassword = (event) => {
-        this.setState({password : event.target.value});
-    }
+  setEmail = (event) => {
+    this.setState({ email: event.target.value });
+  };
+  setPassword = (event) => {
+    this.setState({ password: event.target.value });
+  };
 
-    handleSubmit = (event) => {
-        const data = new FormData(event.target);
+  handleSubmit = (event) => {
+    const data = new FormData(event.target);
 
-        fetch(`http://192.168.0.21:9000/login/`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                'Access-Control-Allow-Origin' : '*'
-            },
-            body: JSON.stringify({ email : data.get('email'), password : data.get('password')})
-        })
-        .then(res => res.json())
-        .then(resJson => {
-            console.log(resJson);
-            resJson.success ? this.onLoginSuccess() : this.onLoginFailure(resJson.errorMessage);
-        })
-        .catch(error => {
-            console.log(error);
-        })
+    fetch(`http://localhost:9000/login/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        email: data.get("email"),
+        password: data.get("password"),
+      }),
+    })
+      .then((res) => res.json())
+      .then((resJson) => {
+        console.log(resJson);
+        resJson.success
+          ? this.onLoginSuccess()
+          : this.onLoginFailure(resJson.errorMessage);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-        event.preventDefault();
-    }
+    event.preventDefault();
+  };
 
-    render() {
-        return (
-            <div className="wrapper">
-                <h1>Login Page</h1>
-                <form className={styles.login_form} onSubmit={this.handleSubmit}>
-                    <label>
-                        <p>Email</p>
-                        <input type="text" name="email" onChange={this.setEmail}></input>
-                    </label>
-                    <label>
-                        <p>Password</p>
-                        <input type="password" name="password" onChange={this.setPassword}></input>
-                    </label>
-                    <div>
-                        <input type="submit" value="Login"></input>
-                    </div>
-                </form>
-                <button onClick={() => window.location.href='/register'}>Register</button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="wrapper">
+        <h1>Login Page</h1>
+        <form className={styles.login_form} onSubmit={this.handleSubmit}>
+          <label>
+            <p>Email</p>
+            <input type="text" name="email" onChange={this.setEmail}></input>
+          </label>
+          <label>
+            <p>Password</p>
+            <input
+              type="password"
+              name="password"
+              onChange={this.setPassword}
+            ></input>
+          </label>
+          <div>
+            <input type="submit" value="Login"></input>
+          </div>
+        </form>
+        <button onClick={() => (window.location.href = "/register")}>
+          Register
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Login;
--- src/Products/Products.js
+++ src/Products/Products.js
@@ -1,148 +1,155 @@
 import styles from "./Products.module.css";
-import ProductForm from './ProductForm';
-import SaveChangesButton from './SaveChangesButton';
-import ProductCard from './ProductCard';
-import React from 'react';
+import ProductForm from "./ProductForm";
+import SaveChangesButton from "./SaveChangesButton";
+import ProductCard from "./ProductCard";
+import React from "react";
 
 class Products extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            error : null,
-            isLoaded : false,
-            isUpdated : true,
-            products : [],
-            productsToAdd : [],
-            productsToUpdate : [],
-            productsToDelete : []
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      isUpdated: true,
+      products: [],
+      productsToAdd: [],
+      productsToUpdate: [],
+      productsToDelete: [],
+    };
+  }
+
+  productAlreadyExists = (productName) => {
+    var sameProductFound = false;
+
+    this.state.products.forEach((product) => {
+      if (product.name === productName) sameProductFound = true;
+    });
+    return sameProductFound;
+  };
+
+  addProduct = (productName) => {
+    if (!this.productAlreadyExists(productName)) {
+      var newProduct = {
+        name: productName,
+        count: 0,
+      };
+      this.state.products.push(newProduct);
+      this.state.productsToAdd.push(newProduct);
+      this.showProducts();
+    }
+  };
+
+  deleteProduct = (productName) => {
+    console.log(productName);
+    this.state.products.forEach((product) => {
+      if (product.name === productName) {
+        var indexOfProductToDelete = this.state.products.indexOf(product);
+        this.state.products.splice(indexOfProductToDelete, 1);
+      }
+    });
+    this.state.productsToDelete.push(productName);
+    this.showProducts();
+  };
+
+  setProductToUpdate = (productToUpdate) => {
+    var productFound = false;
+    this.state.productsToUpdate.forEach((product) => {
+      if (product.name === productToUpdate.name) {
+        product.count = productToUpdate.count;
+        productFound = true;
+      }
+    });
+
+    if (!productFound) this.state.productsToUpdate.push(productToUpdate);
+  };
+
+  saveChanges = () => {
+    if (this.state.productsToAdd.length > 0) {
+      this.state.productsToAdd.forEach((productToAdd) => {
+        fetch(`http://localhost:9000/products/`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            "Access-Control-Allow-Origin": "*",
+          },
+          body: JSON.stringify({ name: productToAdd.name }),
+        });
+      });
+      this.setState({ productsToAdd: [] });
+    }
+
+    if (this.state.productsToUpdate.length > 0) {
+      this.state.productsToUpdate.forEach((productToUpdate) => {
+        fetch(
+          `http://localhost:9000/products/${productToUpdate.name}?count=${productToUpdate.count}`,
+          { method: "PUT" }
+        );
+      });
+      this.setState({ productsToUpdate: [] });
+    }
+
+    if (this.state.productsToDelete.length > 0) {
+      this.state.productsToDelete.forEach((productToDelete) => {
+        fetch(`http://localhost:9000/products/${productToDelete}`, {
+          method: "DELETE",
+        });
+      });
+      this.setState({ productsToDelete: [] });
+    }
+  };
+
+  showProducts = () => {
+    this.forceUpdate();
+  };
+
+  fetchProducts = () => {
+    fetch("http://localhost:9000/products")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            products: result,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
         }
+      );
+  };
+
+  componentDidMount() {
+    this.fetchProducts();
+  }
+
+  render() {
+    const { error, isLoaded, products } = this.state;
+    if (error) {
+      return <div>Erreur : {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Chargement…</div>;
+    } else {
+      return (
+        <div className={styles.wrapper}>
+          <ProductForm addProduct={this.addProduct} />
+          <SaveChangesButton saveChanges={this.saveChanges}></SaveChangesButton>
+          {products.map((product) => (
+            <ProductCard
+              key={product.name}
+              setProductToUpdate={this.setProductToUpdate}
+              deleteProduct={this.deleteProduct}
+              productName={product.name}
+              productCount={product.count}
+            />
+          ))}
+        </div>
+      );
     }
-
-    productAlreadyExists = (productName) => {
-        var sameProductFound = false;
-
-        this.state.products.forEach(product => {
-            if (product.name === productName)
-                sameProductFound = true;
-        })
-        return sameProductFound
-    }
-
-    addProduct = (productName) => {
-        if (!this.productAlreadyExists(productName)) {
-            var newProduct = {
-                name : productName,
-                count : 0
-            }
-            this.state.products.push(newProduct);
-            this.state.productsToAdd.push(newProduct);
-            this.showProducts();
-        }  
-    }
-
-    deleteProduct = (productName) => {
-        console.log(productName)
-        this.state.products.forEach(product => {
-            if (product.name === productName) {
-                var indexOfProductToDelete = this.state.products.indexOf(product);
-                this.state.products.splice(indexOfProductToDelete, 1);
-            }
-        })
-        this.state.productsToDelete.push(productName);
-        this.showProducts();
-    }
-
-    setProductToUpdate = (productToUpdate) => {
-        var productFound = false;
-        this.state.productsToUpdate.forEach(product => {
-            if (product.name === productToUpdate.name) {
-                product.count = productToUpdate.count
-                productFound = true;
-            }
-        })
-
-        if (!productFound)
-            this.state.productsToUpdate.push(productToUpdate);
-        
-    }
-
-    saveChanges = () => {   
-        if (this.state.productsToAdd.length > 0) {
-            this.state.productsToAdd.forEach(productToAdd => {
-                fetch(`http://192.168.0.21:9000/products/`, { method: 'POST', headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json',
-                    'Access-Control-Allow-Origin' : '*'
-                }, body: JSON.stringify({ name: productToAdd.name }) })
-            })
-            this.setState({productsToAdd : []});
-        }
-
-        if (this.state.productsToUpdate.length > 0) {
-            this.state.productsToUpdate.forEach(productToUpdate => {
-                fetch(`http://192.168.0.21:9000/products/${productToUpdate.name}?count=${productToUpdate.count}`, { method: 'PUT' })
-            })
-            this.setState({productsToUpdate : []});
-        }
-
-
-        if (this.state.productsToDelete.length > 0) {
-            this.state.productsToDelete.forEach(productToDelete => {
-                fetch(`http://192.168.0.21:9000/products/${productToDelete}`, { method: 'DELETE' })
-            })
-            this.setState({productsToDelete : []});
-        }
-    }
-
-    showProducts = () => {
-        this.forceUpdate()
-    }
-
-    fetchProducts = () => {
-        fetch("http://192.168.0.21:9000/products")
-        .then(res => res.json())
-        .then(
-            (result) => {
-                this.setState({
-                    isLoaded: true,
-                    products : result
-                });
-            },
-            (error) => {
-                this.setState({
-                    isLoaded: true,
-                    error
-                });
-            }
-        )
-    }
-
-    componentDidMount() {
-        this.fetchProducts();
-    }
-
-    render() {
-        const { error, isLoaded, products } = this.state;
-        if (error) {
-        return <div>Erreur : {error.message}</div>;
-        } else if (!isLoaded) {
-        return <div>Chargement…</div>;
-        } else {
-        return (
-                <div className={styles.wrapper}>
-                    <ProductForm addProduct={this.addProduct} />
-                    <SaveChangesButton saveChanges={this.saveChanges}></SaveChangesButton>
-                    {
-                        products.map(product => (
-                            <ProductCard key={product.name} setProductToUpdate={this.setProductToUpdate} deleteProduct={this.deleteProduct} productName={product.name} productCount={product.count} />
-                        ))
-                    }
-                </div>
-            );
-        }
-    }
+  }
 }
-
-
 
 export default Products;
