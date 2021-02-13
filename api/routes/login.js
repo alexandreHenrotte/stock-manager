d5ca26fdf403978bab024011e165ddbba8cd50b3
Alexandre Henrotte <alexandre.henrotte1@gmail.com>
2021-02-13 21:26:56 +0100

    Add token
    Implement JWT creation on API

--- api/routes/login.js
+++ api/routes/login.js
@@ -1,37 +1,44 @@
-var express = require('express');
-const User = require('./../models/User')
-const bcrypt = require('bcrypt');
+require("dotenv").config();
+var express = require("express");
+const User = require("./../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 var router = express.Router();
 
-router.post('/', async (req, res) => {
-    const user = await User.findOne({email:req.body.email});
-    console.log(req.body);
-    if (user === null) {
-        res.status(400).json({
-            success: false,
-            errorMessage: "User not found"
-        })
-    }
+router.post("/", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  console.log(req.body);
+  if (user === null) {
+    res.status(400).json({
+      success: false,
+      errorMessage: "User not found",
+    });
+  }
 
-    try {
-        if (await bcrypt.compare(req.body.password, user.password)) {
-            res.status(200).json({
-                success: true,
-                redirectUrl: "/products"
-            });
-        }
-        else {
-            res.status(400).json({
-                sucess: false,
-                errorMessage: "Wrong password"
-            })
-        }
-    } catch {
-        return res.status(500).json({
-            success: false,
-            errorMessage: "Authentication Error"
-        })
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      const token = jwt.sign(
+        { username: user.username },
+        process.env.SECRET_TOKEN_KEY
+      );
+      res.status(200).json({
+        success: true,
+        token: token,
+        redirectUrl: "/products",
+      });
+    } else {
+      res.status(400).json({
+        sucess: false,
+        errorMessage: "Wrong password",
+      });
     }
-})
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      errorMessage: "Authentication Error",
+    });
+  }
+});
 
 module.exports = router;
