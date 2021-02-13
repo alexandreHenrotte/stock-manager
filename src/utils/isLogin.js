474ee4ae3ba9820ae18a022a3658726ef146388e
Alexandre Henrotte <alexandre.henrotte1@gmail.com>
2021-02-13 22:03:57 +0100

    isLogin function

--- src/utils/isLogin.js
+++ src/utils/isLogin.js
@@ -1,6 +1,21 @@
+var jwt = require("jsonwebtoken");
+
+const tokenIsExpired = () => {
+  var tokenExpired = false;
+
+  const token = localStorage.getItem("token");
+  var decodedToken = jwt.decode(token, { complete: true });
+  var dateNow = new Date();
+
+  if (decodedToken.exp < dateNow.getTime()) tokenExpired = true;
+
+  return tokenExpired;
+};
 
 const isLogin = () => {
+  if (localStorage.getItem("token") === null || tokenIsExpired()) {
     return false;
-}
+  } else return true;
+};
 
-export {isLogin};
+export default isLogin;
