7ab89f3af1ca0d06414038c4159f2dd4bd977f46
Alexandre Henrotte <alexandre.henrotte1@gmail.com>
2021-02-14 01:13:45 +0100

    Login, Logout, Register buttons navbar update
    If the user is logged in, only "logout" shows up and if he is not, only "login" and "register" appears.

--- src/App/BootstrapNavBar.js
+++ src/App/BootstrapNavBar.js
@@ -1,19 +1,21 @@
 
 import React from 'react';
 import { Navbar, Nav, Form, Button } from 'react-bootstrap';
+import isLogin from '../utils/isLogin';
+import logout from '../utils/logout';
 
 class BootstrapNavBar extends React.Component {
-  render() {
-    return (
-      <div>
-      <Navbar bg="light" expand="lg" style={{ width: '100%' }}>
-				<Navbar.Brand href="/">Stock-Manager</Navbar.Brand>
-				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav className="mr-auto">
-						<Nav.Link href="/stocks">Stocks</Nav.Link>
-						<Nav.Link href="/settings">Settings</Nav.Link>
-						{/*
+	render() {
+		return (
+			<div>
+				<Navbar bg="light" expand="lg" style={{ width: '100%' }}>
+					<Navbar.Brand href="/">Stock-Manager</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="mr-auto">
+							<Nav.Link href="/stocks">Stocks</Nav.Link>
+							<Nav.Link href="/settings">Settings</Nav.Link>
+							{/*
 						<Nav.Link href="#link">Link</Nav.Link>
 						<NavDropdown title="Dropdown" id="basic-nav-dropdown">
 							<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
@@ -23,16 +25,24 @@ class BootstrapNavBar extends React.Component {
 							<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
 						</NavDropdown>
 						*/}
-					</Nav>
-					<Form inline>
-						<Button variant="outline-success mr-2" onClick={() => window.location.href='/login'}>Login</Button>
-						<Button variant="outline-warning" onClick={() => window.location.href='/register'}>Register</Button>
-					</Form>
-				</Navbar.Collapse>
-			</Navbar>
-      </div>
-    )
-  }
+						</Nav>
+
+						{isLogin()
+							? <Form inline><Button variant="outline-danger" onClick={logout}>Logout</Button></Form>
+							: (
+								<Form inline>
+									<Button variant="outline-success mr-2" onClick={() => window.location.href = '/login'}>Login</Button>
+									<Button variant="outline-warning" onClick={() => window.location.href = '/register'}>Register</Button>)
+								</Form>
+							)
+						}
+
+
+					</Navbar.Collapse>
+				</Navbar>
+			</div>
+		)
+	}
 }
 
 export default BootstrapNavBar;
