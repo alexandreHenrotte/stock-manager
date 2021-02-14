2ae6889a992a6ae2350cb618acd370322b28b56c
Alexandre Henrotte <alexandre.henrotte1@gmail.com>
2021-02-14 01:11:39 +0100

    Update isLogin and remove JWT on expire

--- src/utils/isLogin.js
+++ src/utils/isLogin.js
@@ -7,15 +7,26 @@ const tokenIsExpired = () => {
   var decodedToken = jwt.decode(token, { complete: true });
   var dateNow = new Date();
 
-  if (decodedToken.exp < dateNow.getTime()) tokenExpired = true;
+  if (decodedToken !== null && decodedToken.exp < dateNow.getTime()) {
+    localStorage.removeItem('token');
+    tokenExpired = true;
+  }
 
   return tokenExpired;
 };
 
 const isLogin = () => {
-  if (localStorage.getItem("token") === null || tokenIsExpired()) {
+  console.log("a", localStorage.getItem("token") === null);
+  console.log("b", tokenIsExpired())
+  if (localStorage.getItem("token") === null) {
     return false;
-  } else return true;
+  }
+  else if (localStorage.getItem("token") === true && tokenIsExpired()) {
+    return false;
+  }
+  else {
+    return true;
+  }
 };
 
 export default isLogin;
